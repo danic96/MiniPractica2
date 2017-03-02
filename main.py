43103ea5a3e804a15b99ffc4dd7fd90b293993a1
dani <dani.uni1137@gmail.com>
2017-03-02 12:57:11 +0100

    almanac treballa amb json

--- main.py
+++ main.py
@@ -3,8 +3,6 @@
 # vim: set fileencoding=utf8 :
 
 import sys
-import urllib2
-from bs4 import BeautifulSoup as bs
 import json
 import requests
 
@@ -26,27 +24,22 @@ class WeatherClient(object):
     def almanac(self, location):
         # baixar-se la web
         url = self.url_base + self.api_key + self.url_service["almanac"] + \
-            location + ".xml"
+            location + ".json"
         print url
         data = requests.get(url).text
 
         # llegir-la
-        soup = bs(data, 'lxml')
-        maximes = soup.find("temp_high")
-        minimes = soup.find("temp_low")
-        normal_maximes = maximes.find("normal").find("c").text
-        record_maximes = maximes.find("record").find("c").text
-
-        normal_minimes = minimes.find("normal").find("c").text
-        record_minimes = minimes.find("record").find("c").text
+        # soup = bs(data, 'lxml')
+        jsondata = json.loads(data)
+        almanac = jsondata["almanac"]
 
         resultats = {}
         resultats["maximes"] = {}
         resultats["minimes"] = {}
-        resultats["maximes"]["normal"] = normal_maximes
-        resultats["maximes"]["record"] = record_maximes
-        resultats["minimes"]["normal"] = normal_minimes
-        resultats["minimes"]["record"] = record_minimes
+        resultats["maximes"]["normal"] = almanac["temp_high"]["normal"]["C"]
+        resultats["maximes"]["record"] = almanac["temp_high"]["record"]["C"]
+        resultats["minimes"]["normal"] = almanac["temp_low"]["normal"]["C"]
+        resultats["minimes"]["record"] = almanac["temp_low"]["record"]["C"]
 
         # retornar resultats
         return resultats
@@ -58,12 +51,11 @@ if __name__ == "__main__":
             api_key = sys.argv[1]
         except IndexError:
             key_file = open('api_key', 'r')
-            api_key = key_file.read()
-            print "API Key must be in CLI option"
+            api_key = key_file.read().replace('\n', '')
+            print "Must provide api key in code or cmdline arg"
 
     print api_key
-    """
+
     wc = WeatherClient(api_key)
     result = wc.almanac("Lleida")
     print result
-    """
